b771adfa6db47791b6550fd677117ee6bc4e9342
Jus Winny Herivao <winny.herivao@spoonconsulting.com>
2024-01-09 16:50:38 +0300

    update

--- backend/controller/espace_contribuable/autreController.js
+++ backend/controller/espace_contribuable/autreController.js
@@ -5,20 +5,20 @@ const data = {
 
 const setAutres = async (req, res) => {
     const id_contribuable = req.body.id_contribuable;
-    const id = data.autres.length === 0 ? 0 : data.autres[data.autres.length - 1].id + 1;
+    const id_autre = data.autres.length === 0 ? 0 : data.autres[data.autres.length - 1].id_autre + 1;
     const information_mail = req.body.information_mail;
     const depaseement_12_mois = req.body.depaseement_12_mois;
     const certification = req.body.certification;
 
     const autre = {
-        'id': id,
+        'id_autre': id,
         'id_contribuable': id_contribuable,
         'information_mail': information_mail,
         'depassement_12_mois': depaseement_12_mois,
         'certification': certification
     }
 
-    setAutres([...data.autres, autre]);
+    data.setAutres([...data.autres, autre]);
     await fsPromises.writeFile(
         path.join(__dirname, '..', '..', 'model', 'model_temps', 'autre.json'),
         JSON.stringify(data.contribs)
