215c30edbbb51eeb05d8bdeef6a085c67712868f
Jus Winny Herivao <winny.herivao@spoonconsulting.com>
2024-01-09 06:05:48 +0300

    update

--- backend/controller/consultation/consultationController.js
+++ backend/controller/consultation/consultationController.js
@@ -139,11 +139,88 @@ const getListeMiseAJourAValide = (req, res) => {
     contribuables = [];
 }
 
+const getListeMiseAJourAValideByAll = (req, res) => {
+    const reference = req.body.reference;
+    const raison_social = req.body.raison_social;
+    const reference_fiscal = req.body.reference_fiscal;
+    const cin = req.body.cin;
+    const adresse = req.body.adresse;
+    const nom_commercial = req.body.nom_commercial;
+    const date_debut = req.body.date_debut;
+    const date_fin = req.body.date_fin;
+    let contribuable = {};
+
+    if(!date_debut){
+        if(reference && !raison_social && !reference_fiscal && !cin && !adresse && !nom_commercial){
+            contribuable = data.contribuablesNonValide.find(con => con.id === reference);
+        }
+        else if (!reference && raison_social && !reference_fiscal && !cin && !adresse && !nom_commercial){
+            contribuable = data.contribuablesNonValide.find(con => con.raison_social === raison_social);
+        }
+        else if (!reference && !raison_social && reference_fiscal && !cin && !adresse && !nom_commercial){
+            contribuable = data.contribuablesNonValide.find(con => con.reference_fiscal === reference_fiscal);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && cin && !adresse && !nom_commercial){
+            contribuable = data.contribuablesNonValide.find(con => con.cin === cin);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && !cin && adresse && !nom_commercial){
+            contribuable = data.contribuablesNonValide.find(con => con.adresse === adresse);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && !cin && !adresse && nom_commercial){
+            contribuable = data.contribuablesNonValide.find(con => con.nom_commercial === nom_commercial)
+        }
+    } else if(date_debut && !date_fin){
+        const date = (new Date(date_debut)).setDate((new Date(date_debut)).getDate() + 7);
+        if(reference && !raison_social && !reference_fiscal && !cin && !adresse && !nom_commercial){
+            contribuable = data.contribuablesNonValide.find(con => con.id === reference && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date);
+        }
+        else if (!reference && raison_social && !reference_fiscal && !cin && !adresse && !nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date){
+            contribuable = data.contribuablesNonValide.find(con => con.raison_social === raison_social);
+        }
+        else if (!reference && !raison_social && reference_fiscal && !cin && !adresse && !nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date){
+            contribuable = data.contribuablesNonValide.find(con => con.reference_fiscal === reference_fiscal);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && cin && !adresse && !nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date){
+            contribuable = data.contribuablesNonValide.find(con => con.cin === cin);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && !cin && adresse && !nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date){
+            contribuable = data.contribuablesNonValide.find(con => con.adresse === adresse);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && !cin && !adresse && nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date){
+            contribuable = data.contribuablesNonValide.find(con => con.nom_commercial === nom_commercial)
+        }
+    } else if(date_debut && date_fin){
+        if(reference && !raison_social && !reference_fiscal && !cin && !adresse && !nom_commercial){
+            contribuable = data.contribuablesNonValide.find(con => con.id === reference && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date_fin);
+        }
+        else if (!reference && raison_social && !reference_fiscal && !cin && !adresse && !nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date_fin){
+            contribuable = data.contribuablesNonValide.find(con => con.raison_social === raison_social);
+        }
+        else if (!reference && !raison_social && reference_fiscal && !cin && !adresse && !nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date_fin){
+            contribuable = data.contribuablesNonValide.find(con => con.reference_fiscal === reference_fiscal);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && cin && !adresse && !nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date_fin){
+            contribuable = data.contribuablesNonValide.find(con => con.cin === cin);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && !cin && adresse && !nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date_fin){
+            contribuable = data.contribuablesNonValide.find(con => con.adresse === adresse);
+        }
+        else if (!reference && !raison_social && !reference_fiscal && !cin && !adresse && nom_commercial && (new Date(con.date_creation)) >= date_debut && (new Date(con.date_creation)) <= date_fin){
+            contribuable = data.contribuablesNonValide.find(con => con.nom_commercial === nom_commercial)
+        }
+    } else {
+        return res.status(400).json({'message': 'aucun contribuable'})
+    }
+
+    res.json(contribuable);
+}
+
 module.exports = {
     getContribuables,
     getContribuablebloque,
     getContribuableNonBloque,
     getListeDemandeAValide,
     getListeMiseAJourAValide,
-    getListeDemandeAValideByAll
+    getListeDemandeAValideByAll,
+    getListeMiseAJourAValideByAll
 }
--- test.js
+++
@@ -1,8 +0,0 @@
-const data = {
-    "nom": "HH",
-    "prenom": "xx"
-}
-
-data.age = 12
-
-console.log(data);
