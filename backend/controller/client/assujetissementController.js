130046a38182b371a85b3cf7138303968fc39a8b
Jus Winny Herivao <winny.herivao@spoonconsulting.com>
2024-01-13 23:07:03 +0300

    update

--- backend/controller/client/assujetissementController.js
+++ backend/controller/client/assujetissementController.js
@@ -7,44 +7,13 @@ const path = require('path');
 const fsPromises = require('fs').promises;
 
 const setAssujetissement = async (req, res) => {
-    const id_contribuable = req.params.id_contribuable;
-    const imposition = req.body.imposition;
-    const date_debut = req.body.date_debut;
-    const periodicite = req.body.periodicite;
-    const annee = req.body.annee;
-    const actif = req.body.actif;
-    const exonere = req.body.exonere;
-    const period_1 = req.body.period_1;
-    const period_2 = req.body.period_2;
-    const etat = req.body.etat;
-    const date_exe = req.body.date_exe;
-    const date_assujetissement = req.body.date_assujetissement;
-
-    const id = data.assujetissements.length === 0 ? 1 : data.assujetissements[data.assujetissements.length - 1].id + 1;
-
-    const new_imposition = {
-        "id": id,
-        "id_contribuable": id_contribuable,
-        "imposition": imposition,
-        "date_debut": date_debut,
-        "periodicite": periodicite,
-        "annee": annee,
-        "actif": annee,
-        "exonere": exonere,
-        "period_1": period_1,
-        "period_2": period_2,
-        "etat": etat,
-        "date_exe": date_exe,
-        "date_assujetissement": date_assujetissement
-    }
-
+    const new_imposition = req.body.assujetissements;
     data.setAssujetissement([...data.assujetissements, new_imposition]);
 
     await fsPromises.writeFile(
         path.join(__dirname, '..', '..', 'model', 'assujetissement.json'),
         JSON.stringify(data.assujetissements)
     )
-
     res.json(data.assujetissements);
 }
 
