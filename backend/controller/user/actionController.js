9e14294abd5a462bebc101c44e845ec42fd3535d
Jus Winny Herivao <winny.herivao@spoonconsulting.com>
2024-01-14 05:38:29 +0300

    update

--- backend/controller/user/actionController.js
+++ backend/controller/user/actionController.js
@@ -18,9 +18,33 @@ const getActionByAll = (req, res) => {
     const date_debut = req.body.date_debut;
     const date_fin = req.body.date_fin;
     const login = req.body.date_fin;
-
-   
-
+    let user = {};
+    let history = [];
+    if (!date_debut && !date_fin) {
+        if (!login) {
+            data.users.map(use => {
+                data.history.map(his => {
+                    if (use.id === his.id_user) {
+                        history = []
+                        history.push({ ...use, ...his });
+                    }
+                }
+                )
+            })
+        }else {
+            user = data.users.find(use => use.code === login);
+            data.history.map(his => {
+                if(his.id_user === user.id){
+                    history.push({...user, his});
+                }
+            })
+        }
+    }else if(date_debut && !date_fin){
+        if(!login){
+            const tel = 1;
+        }
+    }
+    res.json()
 }
 
 module.exports = {
