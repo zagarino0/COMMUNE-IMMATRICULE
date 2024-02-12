14a88a4f095b4837813dba21bf7b005be488cbd5
Jus Winny Herivao <winny.herivao@spoonconsulting.com>
2024-02-12 12:34:25 +0300

    update

--- backend/controller/user/actionController.js
+++ backend/controller/user/actionController.js
@@ -8,8 +8,8 @@ const getAllAction = (req, res) => {
     let history = [];
     data.users.map(use => {
         data.history.map(his => {
-            if(use.id === his.id_user && (new Date(his.date_history)) >= (new Date(date_debut)) && (new Date(his.date_history)) <= (new Date())){
-                history.push({...use, ...his})
+            if (use.id === his.id_user && (new Date(his.date_history)) >= (new Date(date_debut)) && (new Date(his.date_history)) <= (new Date())) {
+                history.push({ ...use, ...his })
             }
         })
     })
@@ -20,76 +20,70 @@ const getAllAction = (req, res) => {
 const getActionByUserId = (req, res) => {
     const id_user = req.params.id_user;
     const history = data.history.filter(his => his.id_user === id_user);
-    if(!history)
-        return res.status(404).json({'message': 'history not found'});
+    if (!history)
+        return res.status(404).json({ 'message': 'history not found' });
     const user = data.users.find(use => use.id === history.id_user);
-    res.json({...history, ...user});
+    res.json({ ...history, ...user });
 }
 
 const getActionByAll = (req, res) => {
     const date_debut = req.body.date_debut;
     const date_fin = req.body.date_fin;
-    const login = req.body.date_fin;
-    let user = {};
-    let history = [];
-    if (!date_debut && !date_fin) {
-        if (!login) {
-            data.users.map(use => {
-                data.history.map(his => {
-                    if (use.id === his.id_user) {
-                        history = []
-                        history.push({ ...use, ...his });
-                    }
-                }
-                )
+    const code = req.body.code;
+
+    if (!code) {
+        if (!date_debut && !date_fin) {
+            const user = data.users;
+            user.map(use => {
+                const history = data.history.filter(his => his.id_user === use.id_user);
+                use.historique = history;
             })
-        }else {
-            user = data.users.find(use => use.code === login);
-            data.history.map(his => {
-                if(his.id_user === user.id){
-                    history.push({...user, ...his});
-                }
+            res.json(user);
+        } else if (date_debut && !date_fin) {
+            const allUser = data.users;
+            allUser.map(use => {
+                const history = data.history.filter(his => (new Date(his.date_history)) >= (new Date(date_debut)) && his.id_user === use.id_user);
+                use.historique = history;
             })
+            const users = allUser.filter(use => use.historique.length !== 0);
+            res.json(users);
         }
-    }else if(date_debut && !date_fin){
-        if(!login){
-            data.users.map(use => {
-                data.history.map(his => {
-                    if(use.id === his.id_user && (new Date(his.date_history)) >= (new Date(date_debut)) && (new Date(his.date_history)) <= (new Date())){
-                        history.push({...use, ...his})
-                    }
-                })
+        else if (date_debut && date_fin) {
+            const allUser = data.users;
+            allUser.map(use => {
+                const history = data.history.filter(his => (new Date(his.date_history)) >= (new Date(date_debut)) && (new Date(his.date_history)) <= (new Date(date_fin)) && his.id_user === use.id_user);
+                use.historique = history;
             })
-        }else{
-            user = data.users.find(use => use.code === login);
-            data.history.map(his => {
-                if(his.id_user === user.id && (new Date(date_history)) >= (new Date(date_debut)) && (new Date(date_history)) <= (new Date())){
-                    history.push({...user, ...his});
-                }
-            })
+            const users = allUser.filter(use => use.historique.length !== 0);
+            res.json(users);
         }
-    }else if (!date_debut && date_fin){
-        history.push('Aucun historique disponible');
-    }else if (date_debut && date_fin){
-        if(!login){
-            data.users.map(use => {
-                data.history.map(his => {
-                    if(use.id === his.id_user && (new Date(his.date_history)) >= (new Date(date_debut)) && (new Date(his.date_history)) <= (new Date(date_fin))){
-                        history.push({...his, ...use});
-                    }
-                })
+    } else {
+        if (!date_debut && !date_fin) {
+            const user = data.users.filter(use => use.code === code);
+            user.map(use => {
+                const history = data.history.filter(his => his.id_user === use.id_user);
+                use.historique = history;
             })
-        }else{
-            user = data.users.find(use => use.code === login);
-            data.history.map(his => {
-                if(his.id_user === user.id && (new Date(his.date_history)) >= (new Date(date_debut)) && (new Date(his.date_history)) <= (new Date(date_fin))){
-                    history.push({...user, ...his})
-                }
+            res.json(user);
+        } else if (date_debut && !date_fin) {
+            const allUser = data.users.filter(use => use.code === code);;
+            allUser.map(use => {
+                const history = data.history.filter(his => (new Date(his.date_history)) >= (new Date(date_debut)) && his.id_user === use.id_user);
+                use.historique = history;
             })
+            const users = allUser.filter(use => use.historique.length !== 0);
+            res.json(users);
+        }
+        else if (date_debut && date_fin) {
+            const allUser = data.users.filter(use => use.code === code);;
+            allUser.map(use => {
+                const history = data.history.filter(his => (new Date(his.date_history)) >= (new Date(date_debut)) && (new Date(his.date_history)) <= (new Date(date_fin)) && his.id_user === use.id_user);
+                use.historique = history;
+            })
+            const users = allUser.filter(use => use.historique.length !== 0);
+            res.json(users);
         }
     }
-    res.json(history)
-    history = [];
 }
 
 module.exports = {
