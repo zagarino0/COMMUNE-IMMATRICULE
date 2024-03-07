dce41f58a72170420c78714fd2ff5ecd40c3fd57
Jus Winny Herivao <winny.herivao@spoonconsulting.com>
2024-03-07 05:58:13 +0300

    update

--- backend/controller/consultation/consultationController.js
+++ backend/controller/consultation/consultationController.js
@@ -636,21 +636,21 @@ const getContribuableNonBloque = (req, res) => {
 }
 
 const getAllContribuableBloque = (req, res) => {
-    const contribluable = data.contribuablesNonValide;
+    const contribluable = data.contribuables;
     const contribluableBloque = [];
     contribluable.map( con => {
         // if(!data.modifications.find(mod => mod.id_contribuable === con.id && mod.blockage))
         //     return res.status(404).json({'message': 'erreur quoi'})
         const modif = data.modifications.find(mod => mod.id_contribuable == con.id);
         if(modif.blockage){
-            con.actionnaire = data.actionnaireTemps.length === 0 ? null : data.actionnaireTemps.filter(act => act.id_contribuable === con.id);
-            con.dirigeant = data.dirigeantTemps.length === 0 ? null : data.dirigeantTemps.filter(dir => dir.id_contribuable === con.id);
-            con.activite = data.activiteTemps.length === 0 ? null : data.activiteTemps.find(act => act.id_contribuable === con.id);
-            con.autre = data.autreTemps.length === 0 ? null : data.autreTemps.find(aut => aut.id_contribuable === con.id);
-            con.coordonnees = data.coordonneeTemps.length === 0 ? null : data.coordonneeTemps.find(coo => coo.id_contribuable === con.id);
-            con.etablissement = data.etablissementTemps.length === 0 ? null : data.etablissementTemps.filter(eta => eta.id_contribuable === con.id);
-            con.interlocuteur = data.interlocuteurTemps.length === 0 ? null : data.interlocuteurTemps.find(inter => inter.id_contribuable === con.id);
-            con.siege = data.siegeTemps.length === 0 ? null : data.siegeTemps.find(sie => sie.id_contribuable === con.id);
+            con.actionnaire = data.actionnaires.length === 0 ? null : data.actionnaires.filter(act => act.id_contribuable === con.id);
+            con.dirigeant = data.dirigeants.length === 0 ? null : data.dirigeants.filter(dir => dir.id_contribuable === con.id);
+            con.activite = data.activites.length === 0 ? null : data.activites.find(act => act.id_contribuable === con.id);
+            con.autre = data.autres.length === 0 ? null : data.autres.find(aut => aut.id_contribuable === con.id);
+            con.coordonnees = data.coordonnees.length === 0 ? null : data.coordonnees.find(coo => coo.id_contribuable === con.id);
+            con.etablissement = data.etablissements.length === 0 ? null : data.etablissements.filter(eta => eta.id_contribuable === con.id);
+            con.interlocuteur = data.interlocuteurs.length === 0 ? null : data.interlocuteurs.find(inter => inter.id_contribuable === con.id);
+            con.siege = data.siege.length === 0 ? null : data.siege.find(sie => sie.id_contribuable === con.id);
             contribluableBloque.push({...con});
         }
     })
